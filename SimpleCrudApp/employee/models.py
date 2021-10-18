f72f96f395a381374350304f5ec9b5615abf353a
Sundarrajan R <sundarrajan.r@tavant.com>
2021-10-18 22:30:22 +0530

    schema updated

--- SimpleCrudApp/employee/models.py
+++ SimpleCrudApp/employee/models.py
@@ -1,3 +1,20 @@
 from django.db import models
 
-# Create your models here.
+
+
+class Department(models.Model):
+    dept_id   = models.IntegerField(primary_key=True)
+    dept_name = models.CharField(max_length=60)
+
+class Employee(models.Model):
+    emp_id      = models.IntegerField(primary_key=True)
+    emp_name    = models.CharField(max_length=90)
+    department  = models.OneToOneField(Department,related_name="employee_department",on_delete=models.CASCADE)
+
+#a = Department(dept_id = 11,dept_name="InfoTech")
+#obj = Department.objects.all()
+#print(obj)
+
+get_obj = Department.objects.get(dept_id=11)
+new = Employee(emp_id = 1111,emp_name="A",department=get_obj)
+new.save()
