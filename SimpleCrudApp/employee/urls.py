65f282f664ceb427b2831cfd671759206ea93500
Sundarrajan R <sundarrajan.r@tavant.com>
2021-10-21 21:09:59 +0530

    Api updates

--- SimpleCrudApp/employee/urls.py
+++ SimpleCrudApp/employee/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
 from django.conf.urls import url, include
-from .views import Department
+from .views import Department,Employee
 
 urlpatterns = [
-    path('departments/', Department.as_view())
+    path('departments/', Department.as_view()),
+    path('employee/', Employee.as_view())
 ]
--- SimpleCrudApp/employee/views.py
+++ SimpleCrudApp/employee/views.py
@@ -3,8 +3,8 @@ from django.http import HttpResponse
 from django.shortcuts import get_object_or_404
 from rest_framework.views import APIView
 from rest_framework.response import Response
-from rest_framework import status
-from . models import Employee,Department as dept
+from rest_framework import serializers, status
+from . models import Employee as employee,Department as department
 from . serializers import employeeSerializer,departmentSerializer
 
 
@@ -12,6 +12,45 @@ from . serializers import employeeSerializer,departmentSerializer
 class Department(APIView):                  # inherits from an APIView
  
     def get(self, request):
-        obj = dept.objects.all()      # Getting all values
+        '''
+        An API method which returns the all deparments info.
+        '''
+        obj = department.objects.all()      # Getting all values
         serializer = departmentSerializer(obj, many=True)
         return Response(serializer.data, status=200)
+    
+    def post(self, request):
+        ''''
+        An API method which is used to add department info. 
+        '''
+        data = request.data 
+        serializer = departmentSerializer(data = data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data,status=201)
+        return Response(serializer.errors, status=400)
+
+class Employee(APIView):
+    
+    def get(self, request):
+        '''
+        An API method which returns the all employees info.
+        '''
+        obj = employee.objects.all()        # Getting all values 
+        serializer = employeeSerializer(obj,many=True)
+        return Response(serializer.data, status=200)
+
+    def post(self, request):
+        ''''
+        An API method which is used to add employee info. 
+        '''
+        data = request.data 
+        serializer = employeeSerializer(data = data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data,status=201)
+        return Response(serializer.errors, status=400)
+
+
+
+
